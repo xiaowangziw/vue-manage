436615e476d5fb5159302a24b6ccb3c5af6c6068
wangxxiufen <wangxxiufen@wondersgroup.com>
2022-04-20 17:44:05 +0800

    引入router

---
+++ router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+// import Home from '../views/Home'
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/Home.vue')
+
+    }
+
+]
+const router  = new VueRouter({
+    mode: 'history',
+    routes
+
+})
+
+export default router
